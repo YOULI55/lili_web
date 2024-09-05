ae544c46639bc0a32c8f4edcff510ab9d27ebe7c
shenruofeng <shen.ruofeng@qq.com>
2024-09-05 10:27:33 +0800

    add

---
+++ 前端总结/javascript/09实现setInterval.js
@@ -0,0 +1,20 @@
+const mySetInterval = (fn,delay) => {
+	let timer = null;
+	const inerval = () => {
+		fn();
+		timer = setTimeout(inerval,delay);
+	}
+	inerval();
+	return () => {
+		clearTimeout(timer);
+	}
+}
+
+const clear = mySetInterval(() => {
+	console.log('start');
+},1000)
+
+
+setTimeout(() => {
+	clear();
+},5000)
---
+++ 前端总结/javascript/10ES6新增特性.js
@@ -0,0 +1,34 @@
+// 1. let和const
+// let a = 1;
+// const b = 2;
+
+// 2. 解构赋值
+// const [a,b] = [1,2];
+
+// 3. 字符串模板
+// const name = 'jack';
+// const age = 18;
+// console.log(`my name is ${name},age is ${age}`);
+
+// 4. 箭头函数
+// const fn = (a,b) => {
+// 	return a + b;
+// }
+
+// 5. Promise
+
+// 6. async/await
+
+// 7. class
+
+// 8. 模块化
+// export default fn;
+// import fn from './fn.js';
+
+// 9. 拓展运算符
+// const arr = [1,2,3];
+// const arr2 = [...arr];   
+
+// 10. map,set 数据结构
+// const map = new Map();
+// map.set('name','jack');
