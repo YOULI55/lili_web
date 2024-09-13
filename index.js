940ef035cc48b30c47e089fa62a55a408e32e1fb
shenruofeng <shen.ruofeng@qq.com>
2024-09-13 16:44:13 +0800

    1

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-// 现给定一个整数的 多维数组 ，请你返回一个生成器对象，按照 中序遍历 的顺序逐个生成整数。
-// 多维数组 是一个递归数据结构，包含整数和其他 多维数组。
-// 中序遍历 是从左到右遍历每个数组，在遇到任何整数时生成它，遇到任何数组时递归应用 中序遍历 。
-// 示例 1：
-// 输入：arr = [[[6]],[1,3],[]]
-// 输出：[6,1,3]
-// 解释：
-// const generator = inorderTraversal(arr);
-// generator.next().value; // 6
-// generator.next().value; // 1
-// generator.next().value; // 3
-// generator.next().done; // true
-// 示例 2：
-// 输入：arr = []
-// 输出：[]
-// 解释：输入的多维数组没有任何参数，所以生成器不需要生成任何值。
- 
+// const mySetInterval = (fn,delay) => { 
+//     let timer = null;
+// 	const interval = () => {
+// 		fn();
+// 		timer = setTimeout(() => {
+// 			interval();
+// 		},delay)
+// 	}
+//     interval();
+// 	return () => {
+// 		clearTimeout(timer);
+// 	}
+// }
 
-// 提示：
+// const clear1 = mySetInterval(() => {
+// 	console.log(111)
+// },1000)
 
-// 0 <= arr.flat().length <= 105
-// 0 <= arr.flat()[i] <= 105
-// maxNestingDepth <= 105
+// setTimeout(() => {
+//     clear1();
+// }, 5000);
+
+// const clear2 = mySetInterval(() => {
+// 	console.log(222)
+// },1000)
+
+// setTimeout(() => {
+//     clear2();
+// }, 10000);
+
+
+const mySetTimeout = (fn, delay) => {
+    let timer = null;
+    const timeout = () => {
+        timer = setInterval(() => {
+            fn();
+            clearInterval(timer);
+        }, delay)
+    }
+    timeout();
+}
+
+mySetTimeout(() => {
+    console.log('hello')
+}, 1000)
+
+mySetTimeout(() => {
+    console.log('hello')
+}, 2000)
