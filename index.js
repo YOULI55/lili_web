40ddc0816a9f4d38cd33ba5b4c5ccec69a055b8c
lili <1625674964@qq.com>
2024-12-19 14:47:03 +0800

    add

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
+// abcade
 
-
-a:[ture,ture,false]
-b:[ture,ture,false]
-c:[ture,false,ture]
-
-const check = (val,cur,arr) => {
-    let newCur = [...cur];
-    let index = newCur.indexOf(val);
-    let temp = newCur[index];
-    if(index !== -1) {
-        newCur.splice(index, 1);
-    }else {
-        return false;
-    }
-    for(let i = 0;i < arr.length;i ++) {
-        // for(let j = 0;j < newCur.length;j ++) {
-        //     if(!arr[i].includes(newCur[j])) {
-        //         return false;
-        //     }
-        // }
-    }
-    return true;
-}
-
-const fn = (arr) => {
-    let cur = [];
-    for(let a of arr) {
-        cur.push(...a);
-    }
-    cur = [...new Set(cur)];
-    for(let i = 0;i < arr.length;i ++) {
-        for(let j = 0;j < arr[i].length;j ++) {
-            if(check(arr[i][j],cur,arr)) {
-                let index = cur.indexOf(arr[i][j]);
-                if(index !== -1) {
-                    cur.splice(index, 1);
-                }
-            }
-        }
-    }
-    return cur;
-}
+// [a]bcade
+// [ab]cade
+// [abc]ade
+// 滑动窗口的重点，当前s[i] == 'a'，检查画的窗口内有没有 'a'，
+// 如果有，滑动窗口的左边界就一直向右移动，直到没有'a'为止
+// a[bc]ade => a[bca]de
+// a[bcad]e
+// a[bcade]
